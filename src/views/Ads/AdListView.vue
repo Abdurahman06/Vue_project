e418afcbbe89b25448f68b5b8422f9cd6c43e579
Abdurahman06 <habibullaevabdurrahman@gmail.com>
2025-04-16 23:16:41 +0300

    Подключение данных к списку

--- src/views/Ads/AdListView.vue
+++ src/views/Ads/AdListView.vue
@@ -5,24 +5,26 @@
   <h1 class="text--secondary mb-3 mt-3">My ads</h1>
   <v-card v-for="ad in ads" :key="ad.id" class="mb-3" max-width="1000">
 <v-row>
-  <v-col xs="4">
-<v-img :src="ad.src" height="175px" cover>
-</v-img>
+<v-col xs="4">
+<v-img :src="ad.src" height="175px" 
+cover></v-img>
 </v-col>
-
 <v-col xs="8">
-<h2 class="text--primary">{{  }}</h2>
-<p style="height: 85px; overflow: hidden; text-overflow: ellipsis;">{{  }}</p>
+<h2 class="text--primary">{{ ad.title
+ }}</h2>
+<p style="height: 85px; overflow: hidden;
+ text-overflow: ellipsis;">{{ ad.desc 
+}}</p>
 <v-card-actions>
 <v-spacer></v-spacer>
 <v-btn color="primary" variant="text" :to="'/ad/' + ad.id">
- Open
+Open
 </v-btn>
 </v-card-actions>
 </v-col>
-
 </v-row>
 </v-card>
+
 
   </v-col>
   </v-row>
@@ -32,9 +34,39 @@
   
   <script>
   export default {
-  data () {
-  return {
-  }
-  }
+    data () { 
+	return {
+	ads:[
+	{
+		title:"First",
+		desc:"First Desc",
+		promo: true,
+src:"https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+		id:"1"
+	},
+	{
+		title:"Second",
+		desc:"Second Desc",
+		promo: true,
+		src:"https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+		id:"2"
+	},
+	{
+		title:"Third",
+		desc:"Thitd Desc",
+		promo: true,
+		src:"https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+		id:"3"
+	},
+	{
+		title:"Fouth",
+		desc:"Fouth Desc",
+		promo: true,
+		src:"https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+		id:"4"
+	}
+	]
+} 	
+}
   }
   </script>  
