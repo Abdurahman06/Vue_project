5b49c404dbdd56d495bc0ba9f1b507b166f07ee7
Abdurahman06 <habibullaevabdurrahman@gmail.com>
2025-04-20 14:43:18 +0300

    Подключение лоудера на экране регистрации

--- src/views/Auth/RegistrationView.vue
+++ src/views/Auth/RegistrationView.vue
@@ -15,9 +15,14 @@
                   </v-card-text>
                   <v-card-actions>
                       <v-spacer></v-spacer>
-                      <v-btn color="primary" @click="onSubmit" :disabled="!valid">
-                          Create Account
-                      </v-btn>
+                      <v-btn 
+	color="primary"
+	@click="onSubmit"
+	:loading="loading"
+	:disabled="!valid || loading"	>
+	Create Account
+</v-btn>
+
                   </v-card-actions>
               </v-card>
           </v-col>
@@ -47,6 +52,12 @@ export default {
           ]
       }
   },
+  computed: {
+	loading() {
+		return this.$store.getters.loading
+	}
+},
+
   methods: {
     onSubmit(){
 	if (this.$refs.form.validate()){
