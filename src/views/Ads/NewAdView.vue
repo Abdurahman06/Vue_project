440ae4d85ad31eac05789ccef9c40ea65535ba7f
Abdurahman06 <habibullaevabdurrahman@gmail.com>
2025-04-20 14:20:38 +0300

    Отправка нового объявления

--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -53,16 +53,18 @@ export default {
         }
     },
     methods: {
-        createAd() {
-            if (this.$refs.form.validate()) {
-                const ad = {
-                    title: this.title,
-                    desc: this.description,
-                    promo: this.promo
-                }
-                console.log(ad)
-            }
-        }
+        createAd(){
+	if (this.$refs.form.validate()){
+		const ad = {
+			title: this.title,
+			desc: this.description,
+			promo: this.promo,
+			src: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
+		}
+	this.$store.dispatch("createAd", ad)
+}
+}
+
     }
 }
 </script>
