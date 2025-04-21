6950258f69130a521fe25b43916b7e9b4c557649
Abdurahman06 <habibullaevabdurrahman@gmail.com>
2025-04-21 15:25:13 +0300

    Подключение лоудера на экране создания объявления

--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -33,8 +33,13 @@
   <v-row>
   <v-col cols="8">
   <v-spacer></v-spacer>
-  <v-btn color="success" @click="createAd" :loading="loading"
-    :disabled:="!valid || loading">Create Ad</v-btn>
+  <v-btn
+color="success"
+@click="createAd"
+:loading="loading"
+:disabled:="!valid || loading"
+>Create Ad</v-btn>
+
   </v-col>
   </v-row>
   </v-col>
@@ -65,6 +70,12 @@ export default {
 }
 }
 
-    }
+    },
+    computed: {
+loading() {
+		return this.$store.getters.loading
+	}
+},
+
 }
 </script>
