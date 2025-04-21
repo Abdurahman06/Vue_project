2cd12d82e3f0e5906965828eec7608a35e987207
Abdurahman06 <habibullaevabdurrahman@gmail.com>
2025-04-21 11:35:43 +0300

    Защита роутера

--- src/router/auth_guard.js
+++ src/router/auth_guard.js
@@ -1,9 +1,7 @@
 import store from '../store'
 
 export default function(to,from,next) {
-   if (store.getters.user) {
-   	next()
-   } else {
-   	next('/login?loginError=true')
+   if (store.getters.user) { next()
+   } else { next('/login?loginError=true')
    }
 }
