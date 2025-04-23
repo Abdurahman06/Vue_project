7b9603b793149e3d73639d622cc7fe423810ad80
Abdurahman06 <habibullaevabdurrahman@gmail.com>
2025-04-23 18:25:08 +0300

    Создание store заказов

--- src/store/index.js
+++ src/store/index.js
@@ -2,18 +2,14 @@ import { createStore } from 'vuex'
 import ads from './ads'
 import user from './user'
 import shared from './shared'
-
+import order from './orders'
 
 
 const store = createStore({
-modules:{
-ads, user, shared
-}
-})
-
-
-
-
-
-export default store
+    modules:{
+    ads, user, shared, order
+    }
+    })
+    
+    export default store
 
