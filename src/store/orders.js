06de30f6c26539f118cec5707e0c792bd2ff1918
Abdurahman06 <habibullaevabdurrahman@gmail.com>
2025-04-23 18:30:13 +0300

    Подключение заказов на экране заказов

--- src/store/orders.js
+++ src/store/orders.js
@@ -47,6 +47,12 @@ export default {
             
             
     },
-	getters: {}
+	getters: {
+        orders (state, getters) {
+        if (getters.user == null) return []
+        return state.orders.filter(order => order.userId == getters.user.id)
+        }
+        }
+        
 }
 
